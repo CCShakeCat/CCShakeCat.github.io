35dee76f92aa896e6fd30702456ec32cdca957ad
Mimi Cat <mntnow@gmail.com>
2025-05-25 21:48:48 -0400

    Update whats-new.js

--- stopwatch/whats-new.js
+++ stopwatch/whats-new.js
@@ -2,7 +2,7 @@
 const WHATS_NEW_DATA = {
   version: "1.2.11",
   changelog: [
-    "Fixed the CLOSE SETTINGS button. It now uses a hue shifted ❎ Cross Mark Button"
+    "Fixed the CLOSE SETTINGS button. It now uses a hue shifted ❎ Cross Mark Button\n\nFor more info, visit Fixed the CLOSE SETTINGS button. It now uses a hue shifted ❎ Cross Mark Button"
   ]
 };
 
